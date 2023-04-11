0ddb13ac930df9553ace9049ac01dc38ca5c6b9c
RileyXX <49823202+RileyXX@users.noreply.github.com>
2023-04-11 06:39:56 -0500

    Update WalgreensAutoCouponClipper.js

--- WalgreensAutoCouponClipper.js
+++ WalgreensAutoCouponClipper.js
@@ -1,16 +1,26 @@
+// Get all elements with the class 'btn'
 const clipElements = document.querySelectorAll('.btn');
+// Start with index 0
 let i = 0;
+
+// Recursive function to click elements with delay
 function clickElementWithDelay(i) {
+    // If there are elements left to click
     if (i < clipElements.length) {
-        if(clipElements[i].textContent.indexOf('Clip')>-1){
-			// Set a timeout function to wait for a random time between 300ms to 1800ms
+        // Check if the element contains the text 'Clip'
+        if (clipElements[i].textContent.indexOf('Clip') > -1) {
+            // Schedule a click after a random delay between 500-4000 milliseconds
             setTimeout(() => {
                 clipElements[i].click();
+                // Call the function recursively with the next index
                 clickElementWithDelay(i + 1);
-            }, Math.floor(Math.random()*(1800-300+1)+300));
-        }else{
+            }, Math.floor(Math.random() * (1800 - 300 + 1) + 300));
+        } else {
+            // If the element does not contain the text 'Clip', skip it
             clickElementWithDelay(i + 1);
         }
     }
 }
+
+// Start the recursive function with index 0
 clickElementWithDelay(i);
